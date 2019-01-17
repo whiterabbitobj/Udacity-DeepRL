65976d53ef30e9acd779fad4bb717a640d882157
Matthew Doll <mattdoll@gmail.com>
2019-01-17 16:20:24 -0600

    All work implemented, but getting -inf values for reward returns.

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -5,20 +5,33 @@ from collections import defaultdict
 
 class Agent:
 
-    def __init__(self, nA=6, e_start=1, e_decay=.9999, e_min=0.075, gamma=1.0, lr=1):
+    def __init__(self, nA=6, gamma=1.0, alpha=1,
+                 epsilon_start=1, epsilon_decay=.999999, epsilon_min=0.075):
         """ Initialize agent.
 
         Params
         ======
         - nA: number of actions available to the agent
+        - gamma: Discount rate (probably not implemented)
+        - alpha: learning-rate multiplier
+        - epsilon_start: value epsilon will hold on first episode
+        - epsilon_decay: rate at which epsilon approaches nil
+        - epsilon_min: minimum value of epsilon to guarantee there will always
+            be some exploration over pure-greedy exploitation
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
-        self.epsilon = e_start
-        self.epsilon_decay = e_decay
-        self.epsilon_min = e_min
+        self.epsilon = epsilon_start
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
         self.gamma = gamma
-        self.alpha = lr
+        self.alpha = alpha
+
+    def get_probs(self, state_values):
+        self.epsilon = self.epsilon * self.epsilon_decay
+        probs = np.ones(self.nA) * self.epsilon / self.nA
+        probs[np.argmax(state_values)] = 1 - self.epsilon + (self.epsilon / self.nA)
+        return probs
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -31,7 +44,12 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+
+        if state in self.Q:
+            prob = self.get_probs(self.Q[state])
+        else:
+            prob = np.ones(self.nA) / self.nA
+        return np.random.choice(np.arange(self.nA), p = prob)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -44,7 +62,8 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
-
-
-print("Hello world")
+        q_value = self.Q[state][action]
+        q_value_next = np.max(self.Q[next_state]) if not done else 0
+        g = reward + self.gamma * q_value_next - q_value
+        self.Q[state][action] = q_value + self.alpha * g
+        return
