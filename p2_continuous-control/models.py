9144c051207a2e90d6a8accf1783eb76412d1d3e
Matthew Doll <mattdoll@gmail.com>
2019-03-02 13:09:18 -0600

    Implemented Actor/Critic networks and initializations, next is to determine training algorithms

--- p2_continuous-control/models.py
+++ p2_continuous-control/models.py
@@ -2,33 +2,83 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 
+def initialize_weights(net, low, high):
+    for param in net.parameters():
+        param.data.uniform_(low, high)
 
 class ActorNet(nn.Module):
     """
     Actor network that approximates the non-linear function π(θ)
     """
-    def __init__(self, state_size, action_size, layer_sizes=[400,300]):
+    def __init__(self,
+                 state_size,
+                 action_size,
+                 layer_sizes = [400,300],
+                 weight_low = -3e-3,
+                 weight_high = 3e-3):
         super(ActorNet, self).__init__()
 
-        self.hidden_layers = nn.ModuleList([nn.Linear(state_size, layer_sizes[0])])
-        inner_sizes = zip(layer_sizes[:-1], layer_sizes[1:])
-        self.hidden_layers.extend([nn.Linear(i, o) for i, o in inner_sizes])
-        self.output = nn.Linear(layer_sizes[-1], action_size)
+        #currently errors if user were to provide a custom layer_sizes array
+        #with dimensions other than 2x1
+        fc1, fc2 = layer_sizes
+
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1, fc2)
+        self.output = nn.Linear(fc2, action_size)
+        initialize_weights(self, weight_low, weight_high)
 
     def forward(self, state):
         """
         Build a network that maps state -> action values.
         """
-        x = F.relu(self.hidden_layers[0](state))
-        for layer in self.hidden_layers[1:]:
-            x = F.relu(layer(x))
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
         x = self.output(x).tanh()
         return x
 
+
+
 class CriticNet(nn.Module):
     """
-    Actor network that approximates the non-linear function π(θ)
+    Critic network that approximates the Value of the suggested Actions that
+    come from the Actor network.
+
+    Utilizes the actions as an input to the second hidden layer in order to
+    approximate the continuous control problem.
     """
-    def __init__(self, state_size, actions, layer_sizes=[400,300]):
-        super(ActorNet, self).__init__()
+    def __init__(self,
+                 state_size,
+                 action_size,
+                 n_atoms = 128,
+                 layer_sizes = [400,300],
+                 weight_low = -3e-3,
+                 weight_high = 3e-3):
+        super(CriticNet, self).__init__()
+
+        #currently errors if user were to provide a custom layer_sizes array
+        #with dimensions other than 2x1
+        fc1, fc2 = layer_sizes
+
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1 + action_size, fc2)
+        self.logits = nn.Linear(fc2, n_atoms)
+        self.atoms = torch.linspace(-1, 1, n_atoms)
+        initialize_weights(self, weight_low, weight_high)
+
+    def forward(self, state, actions):
+        """
+        Build a network that maps state -> action values.
+        """
+        x = F.relu(self.fc1(state))
+        x = torch.cat([x, actions], dim=1)
+        x = F.relu(self.fc2(x))
+        x = self.logits(x)
+        self.y = nn.Softmax()(x)
+        self.z = F.softmax(x, dim=1)
+        #self.Q_val = (y * self.atoms).sum()
+        #dist = torch.distributions.Categorical(logits=x)
+        #print(dist)
+        #x = dist
+        return x
