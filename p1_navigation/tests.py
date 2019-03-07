4bf2c92c8893e5408130eac5246fbe0a7d2ccb17
Matthew Doll <mattdoll@gmail.com>
2019-03-07 14:13:28 -0600

    quick checkin to master before merging much updated branch into stream

---
+++ p1_navigation/tests.py
@@ -0,0 +1,18 @@
+import time
+import numpy as np
+import torch
+import progressbar
+
+# from unityagents import UnityEnvironment
+from agent import Agent
+import utils
+from get_args import get_args
+from PIL import Image
+import torchvision.transforms as T
+from collections import namedtuple, deque
+from progress.bar import Bar
+from progress.spinner import Spinner
+
+
+
+3e5
