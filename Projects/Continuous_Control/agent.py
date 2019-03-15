8e88da21e5602b12c0c619ce1537f0a4a3fd191b
Matthew Doll <mattdoll@gmail.com>
2019-03-15 12:05:32 -0500

    Implemented floating point precision adjustment for categorical implementation to avoid, apparently, issues with extremely small decimal places rounding up with CEIL operation and causing a fault on high-precision gpus or all cpus

--- Projects/Continuous_Control/agent.py
+++ Projects/Continuous_Control/agent.py
@@ -260,7 +260,12 @@ class D4PG_Agent:
         projected_atoms = rewards + gamma**rollout * atoms.unsqueeze(0)
         projected_atoms.clamp_(vmin, vmax)
         b = (projected_atoms - vmin) / delta_z
-
+        #
+        precision = 3
+        b = torch.round(b * 10**precision) / 10**precision
+        print(b.max())
+        # for i in b:
+        #     print(i, end="")
         lower_bound = b.floor()
         upper_bound = b.ceil()
 
