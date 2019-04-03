42cf53893568864490f310e798e58285346c7aa8
Matthew Doll <mattdoll@gmail.com>
2019-04-02 20:06:31 -0500

    formatting fixes

--- Projects/Navigation/utils.py
+++ Projects/Navigation/utils.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
 import os.path
-
-
 
 def print_bracketing(info=None, do_upper=True, do_lower=True):
     """
@@ -28,3 +27,45 @@ def check_dir(dir):
 
     if not os.path.isdir(dir):
         os.makedirs(dir)
+
+################################################################################
+# The functions below are primarily for use with the Jupyter Notebook attached #
+# to this project and can be safely disregarded if not touching the IPYNB file #
+################################################################################
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from agent import DQN_Agent
+from data_handling import Saver, Logger
+
+def print_args(args):
+    print('\n'.join(["{}: {}".format(arg, getattr(args, arg)) for arg in vars(args)]))
+
+def plot_results(imdir):
+    images = [os.path.join(imdir, file) for file in os.listdir(imdir) if os.path.splitext(file)[1] == '.png']
+    num = len(images)
+    for img in images:
+        display(Image.open(img))
+
+def print_env_info(state, action, reward):
+    print("The agent chooses ACTIONS that look like:\n{}\n".format(action))
+    print("The environment returns STATES that look like:\n{}\n".format(state))
+    print("The environment returns REWARDS that look like:\n{}".format(reward))
+
+def notebook_eval_agent(args, env, filename, num_eps=2):
+    eval_agent = DQN_Agent(env.state_size, env.action_size, args)
+    eval_saver = Saver(eval_agent.framework, eval_agent, args.save_dir, filename)
+    args.eval = True
+    logger = Logger(eval_agent, args)
+    for episode in range(3):
+        env.reset()
+        state = env.state
+        for t in range(args.max_steps):
+            action = eval_agent.act(state)
+            next_state, reward, done = env.step(action)
+            state = next_state
+            logger.log(reward, eval_agent)
+            if done:
+                break
+        eval_agent.new_episode()
+        logger.step(episode)
